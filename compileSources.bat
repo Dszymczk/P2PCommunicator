3dd9678550f28828c1f39d37407bfd6d3d729db7
Dszymczyk <damian.szymczyk1@gmail.com>
2021-03-24 22:31:56 +0100

    Provide batch scripts for compiling and running application

---
+++ compileSources.bat
@@ -0,0 +1,2 @@
+mkdir target
+javac src/P2PCommunicator.java -classpath ".\src;.\dependencies\jackson-annotations-2.12.2.jar;.\dependencies\jackson-core-2.12.2.jar;.\dependencies\jackson-databind-2.12.2.jar" -d target/
